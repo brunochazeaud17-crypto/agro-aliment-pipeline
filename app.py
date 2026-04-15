ba04e51e4b5f2d5a4b21ec2d4202e3d9ca22466a
Bruno CHAZEAUD <brunochazeaud17@gmail.com>
2026-04-15 16:45:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ with st.expander("📌 A propos de ce projet - Cliquez pour ouvrir/fermer", expa
         - YARA est le **1er producteur mondial** d'engrais azotés d'origine européenne
         - Le gaz naturel représente **70 à 80%** du coût de production d'une tonne d'ammoniac
         - *Par exemple : Quand le gaz passe de 30€ à 100€/MWh, le coût de production d'une tonne d'ammoniac augmente de +200 à 250€*
-        - L'Europe importe encore **30% de ses engrais azotés** de Russie et Biélorussie
+        - L'Europe importe encore **30%** de ses engrais azotés de Russie et Biélorussie
         
         ### Les autres maillons fragiles
         - **Grupa Azoty** (Pologne) : très exposé au gaz russe via le gazoduc Yamal
